efe1390cd3d30659268455ea1549fdb5877f9f6d
John MacFarlane <jgm@berkeley.edu>
2016-11-21 10:21:40 +0100

    Better error messages.
    - informative message if tex2svg not in path
    - if tex2svg has a parse error, the formula is printed,
      and the math node is not transformed (so that generally
      plain tex will appear in the output).

--- pandoc-tex2svg.hs
+++ pandoc-tex2svg.hs
@@ -1,9 +1,14 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 import Text.Pandoc.JSON
+import Control.Monad (when)
+import System.Exit
 import System.Process
 import Data.IORef
 import qualified Data.Map as M
-
+import System.Directory (findExecutable)
+import System.IO (stderr, hPutStrLn)
 
 newtype Cache = Cache (M.Map (MathType, String) String)
 
@@ -20,6 +25,12 @@ main = do
 
 tex2svg :: IORef Cache -> Inline -> IO Inline
 tex2svg cr (Math mt math) = do
+  mbfp <- findExecutable "tex2svg"
+  when (mbfp == Nothing) $ do
+    hPutStrLn stderr $ "The tex2svg program was not found in the path.\n" ++
+        "Install MathJax-node (https://github.com/mathjax/MathJax-node)\n" ++
+        "and ensure that tex2svg is in your path."
+    exitWith $ ExitFailure 1
   cache <- readIORef cr
   svg <- case lookupCache (mt, math) cache of
                Just s  -> return s
@@ -28,9 +39,13 @@ tex2svg cr (Math mt math) = do
                     (["--inline" | mt == InlineMath] ++ [math]) ""
                  modifyIORef cr (addToCache (mt, math) svg')
                  return svg'
-  return $ RawInline (Format "html") $
-    "<span class=\"math " ++
-    (if mt == InlineMath then "inline" else "display") ++ "\">" ++
-    svg ++ "</span>"
+  if null svg -- indicates an error -- tex2svg doesn't return error status
+     then do
+       hPutStrLn stderr $ "Could not convert: " ++ math
+       return $ Math mt math
+     else return $ RawInline (Format "html") $
+               "<span class=\"math " ++
+               (if mt == InlineMath then "inline" else "display") ++ "\">" ++
+               svg ++ "</span>"
 tex2svg _ il = return il
 
